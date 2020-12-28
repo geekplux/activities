773bb67a6455bed8dfd9bfd279ea41a98792ce05
geekplux <geekplux@gmail.com>
2020-12-28 16:25:04 +0800

    chore: make all activities display

--- src/utils/const.js
+++ src/utils/const.js
@@ -11,7 +11,7 @@ const MUNICIPALITY_CITIES_ARR = [
 ];
 
 // IF you outside China please make sure IS_CHINESE = false
-const IS_CHINESE = true;
+const IS_CHINESE = false;
 const CHINESE_INFO_MESSAGE = (yearLength, year) =>
   `我用 App 记录自己跑步 ${yearLength} 年了，下面列表展示的是 ${year} 的数据`;
 const ENGLISH_INFO_MESSAGE = (yearLength, year) =>
